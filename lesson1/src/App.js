c2601eed13606f005bd19e5f3a805648274ae19f
frllk <1402805300@qq.com>
2020-07-27 22:53:00 +0800

    高阶组件及高阶组件的链式调用

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContextPage from './Context/ContextPage';
+import HocPage from './HOC/HocPage';
 
 
 
 function App () {
   return (
     <div className="App">
-      <ContextPage />
+      {/* <ContextPage /> */}
+      <HocPage />
     </div>
   );
 }
---
+++ lesson1/src/HOC/HocPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+// hoc: 是⼀个函数，接收⼀个组件，返回另外⼀个组件
+//这⾥⼤写开头的Cmp是指function或者class组件
+const foo = Cmp => props => {
+  return (
+    <div className="border">
+      <Cmp {...props} />
+    </div>
+  );
+};
+
+const foo2 = Cmp => props => {
+  return (
+    <div className="greenBorder">
+      <Cmp {...props} />
+    </div>
+  );
+};
+
+// const foo = Cmp => {
+// return props => {
+// return (
+// <div className="border">
+// <Cmp {...props} />
+// </div>
+// );
+// };
+// };
+function Child (props) {
+  return <div> Child {props.name}</div>;
+}
+
+// const Foo = foo(Child);
+// 链式调用
+const Foo = foo2(foo(foo(Child)));
+
+export default class HocPage extends Component {
+  render () {
+    return (
+      <div>
+        <h3>HocPage</h3>
+        <Foo name="msg" />
+      </div>
+    );
+  }
+}
