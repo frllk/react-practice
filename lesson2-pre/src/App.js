68a05a008e1420765db5e011790b162e7e4c627e
frllk <1402805300@qq.com>
2020-07-28 21:27:08 +0800

    Redux预习

---
+++ lesson2-pre/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReduxPage from './pages/redux/ReduxPage';
+
+function App () {
+  return (
+    <div className="App">
+      {/* redux */}
+      <ReduxPage />
+    </div>
+  );
+}
+
+export default App;
