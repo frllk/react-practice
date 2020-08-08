042ddc1960d6b7f08b34455570bd2d01edd72a45
frllk <1402805300@qq.com>
2020-08-08 11:47:21 +0800

    ls6 umi

---
+++ lesson6-umi/.umirc.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{
+    path: '/',
+    component: '@/pages/layout/index',
+    routes: [
+      { path: '/', component: '@/pages/index' },
+      { path: '/about', component: '@/pages/about' },
+      { path: '/more', component: '@/pages/more/index' },
+      {
+        path: '/product',
+        component: '@/pages/product/_layout',
+        routes: [
+          {
+            path: '/product/:id',
+            component: '@/pages/product/[id]'
+          },
+        ]
+      },
+      { component: '@/pages/404' },
+    ],
+  }],
+});
