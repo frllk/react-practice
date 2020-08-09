a3035f2473859a27ecffbee1c4173b67a0add8af
frllk <1402805300@qq.com>
2020-08-09 19:25:02 +0800

    ls7 init

---
+++ lesson7/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const jsx = (
+  <div className="border">
+    <p>全栈</p>
+    <a href="https://www.kaikeba.com/">开课吧</a>
+  </div>
+);
+
+ReactDOM.render(
+  jsx,
+  document.getElementById('root')
+);
